7ef243b4d570a4916c1d50c3be6a7cc85c907f4b
RAKIBURRAHMAN007 <ratulrakibur5@gmail.com>
2025-01-23 23:30:13 +0600

    showing top scholarship in homepage complete

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,13 +4,16 @@ import Welcome from '../../components/WelcomeSection/Welcome';
 import HomeAboutUs from '../../components/HomeAboutUs/HomeAboutUs';
 import MainHomeAbout from '../../components/HomeAboutUs/MainHomeAbout';
 import CountUp from '../../components/CountUp/CountUpStat';
+import TopScholarship from '../../components/TopScholarship/Topscholarship';
 
 
 const Home = () => {
     return (
         <div className='space-y-36'>
             <Banner></Banner>
+
             <HomeAboutUs></HomeAboutUs>
+            <TopScholarship></TopScholarship>
             <MainHomeAbout></MainHomeAbout>
             <CountUp></CountUp>
             <Welcome></Welcome>
